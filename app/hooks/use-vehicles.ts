4035f0de277878447b0495341ba9b7e835ab38d5
jonathanferreras <jonathanferreras@icloud.com>
2026-06-13 11:02:47 -0400

    feat(vehicles-hook): add hook to fetch vehicles data from endpoint

---
+++ app/hooks/use-vehicles.ts
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+
+export const useVehicles = () => {
+  const [vehicles, setVehicles] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    const fetchVehicles = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch("/api/v1/vehicles");
+
+        if (!response.ok) {
+          throw new Error("Request for vehicles failed!");
+        }
+        const data = await response.json();
+
+        if (!data) {
+          throw new Error("No vehicles returned.");
+        }
+        setVehicles(data.vehicles);
+      } catch (error) {
+        setError(error);
+      }
+
+      setLoading(false);
+    };
+
+    if (vehicles.length === 0) {
+      fetchVehicles();
+    }
+  }, [vehicles]);
+
+  return { vehicles, loading, error };
+};
